f48ec8aaede78aa9e7e468b2ca7151f69b4118c2
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-07 23:25:13 +0300

    Added Interface for basic media elements

---
+++ Mediateka/Mediateka.Core/Abstract/Interfaces/IMediaElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateka.Core.Abstract.Interfaces
+{
+    public interface IMediaElement
+    {
+        string Name { get; }
+        byte Size { get; }
+    }
+}
