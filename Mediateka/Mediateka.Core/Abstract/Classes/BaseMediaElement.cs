c6ee09302f28aa45d0280668243cd5b2010b7a38
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-08 00:25:39 +0300

    Added base media element class
    class is abstract. For features elements

---
+++ Mediateka/Mediateka.Core/Abstract/Classes/BaseMediaElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediateka.Core.Abstract.Interfaces;
+using Mediateka.Core.Concrete;
+
+namespace Mediateka.Core.Abstract.Classes
+{
+    public abstract class BaseMediaElement : IMediaElement
+    {
+        protected BaseMediaElement(string name, MediaElementProperty property)
+        {
+            Name = name;
+            ElemetProperty = property;
+        }
+
+        public string Name { get; }
+        public MediaElementProperty ElemetProperty { get; }
+    }
+}
