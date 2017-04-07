26c7a1fa4e66c67c2561fab7a85689af3ff04652
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-08 00:26:56 +0300

    Added media element property class
    contain all properties off basic media elements

---
+++ Mediateka/Mediateka.Core/Concrete/MediaElementProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateka.Core.Concrete
+{
+    public class MediaElementProperty
+    {
+        public string Author { get; }
+        public DateTime CreatedDate { get; }
+        public string Path { get; }
+        public byte Size { get; }
+        public string Extension { get; }
+    }
+}
