9a29eec8f52841f270af13c336b7ab911a9ffd6c
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-08 20:53:51 +0300

    Changed argument in audio element constructor

--- Mediateka/Mediateka.Core/Concrete/MediaElements/AudioElement.cs
+++ Mediateka/Mediateka.Core/Concrete/MediaElements/AudioElement.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Mediateka.Core.Abstract.Classes;
 using Mediateka.Core.Abstract.Interfaces;
+using Mediateka.Core.Concrete.MediaProperties;
 
 namespace Mediateka.Core.Concrete.MediaElements
 {
     public class AudioElement:BaseMediaElement, IHasDuration, IPlayable
     {
-        public AudioElement(string name, BaseMediaElementProperty property, TimeSpan duration) 
+        public AudioElement(string name, AudioProperty property, TimeSpan duration) 
             : base(name, property)
         {
             Duration = duration;
