21b05fa8f1faad2eede534e519d7cc215aff1378
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-08 20:21:02 +0300

    Added simple AudioElement class
    class describe all audio elements in mediateka

---
+++ Mediateka/Mediateka.Core/Concrete/MediaElements/AudioElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediateka.Core.Abstract.Classes;
+using Mediateka.Core.Abstract.Interfaces;
+
+namespace Mediateka.Core.Concrete.MediaElements
+{
+    public class AudioElement:BaseMediaElement, IHasDuration, IPlayable
+    {
+        public AudioElement(string name, BaseMediaElementProperty property, TimeSpan duration) 
+            : base(name, property)
+        {
+            Duration = duration;
+        }
+
+        public TimeSpan Duration { get; }
+        public string Play()
+        {
+            return string.Format("Playing Audio Track: {0}", Name);
+        }
+    }
+}
