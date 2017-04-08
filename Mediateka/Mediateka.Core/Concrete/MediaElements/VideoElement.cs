0dc8cc06d83c4bd737491ea1177f71f66f78e523
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-08 20:54:24 +0300

    Added VideoElement class
    describe all video files

---
+++ Mediateka/Mediateka.Core/Concrete/MediaElements/VideoElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediateka.Core.Abstract.Classes;
+using Mediateka.Core.Abstract.Interfaces;
+using Mediateka.Core.Concrete.MediaProperties;
+
+namespace Mediateka.Core.Concrete.MediaElements
+{
+    public class VideoElement:BaseMediaElement, IHasDuration, IPlayable
+    {
+        public VideoElement(string name, VideoProperty property, TimeSpan duration) 
+            : base(name, property)
+        {
+            Duration = duration;
+        }
+
+        public TimeSpan Duration { get; }
+        public string Play()
+        {
+            return string.Format("Playing Video File: {0}", Name);
+        }
+    }
+}
