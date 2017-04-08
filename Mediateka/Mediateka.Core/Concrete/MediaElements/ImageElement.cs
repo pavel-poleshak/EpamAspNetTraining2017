bfeb51f59e1c5aa0c92c0fb3a273812a3c0582ce
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-08 21:03:42 +0300

    Added ImageElement class
    describe images in mediateka

---
+++ Mediateka/Mediateka.Core/Concrete/MediaElements/ImageElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediateka.Core.Abstract.Classes;
+using Mediateka.Core.Abstract.Interfaces;
+using Mediateka.Core.Concrete.MediaProperties;
+
+namespace Mediateka.Core.Concrete.MediaElements
+{
+    public class ImageElement:BaseMediaElement, IShowable
+    {
+        public ImageElement(string name, ImageProperty property) : base(name, property)
+        {
+        }
+
+        public string Show()
+        {
+            return string.Format("Showing the image: {0}", Name);
+        }
+    }
+}
