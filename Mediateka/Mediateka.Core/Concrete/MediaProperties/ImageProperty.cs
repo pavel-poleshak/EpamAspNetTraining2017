484866b934e42d5becd69b2b8bf0979752842432
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-08 20:07:57 +0300

    Added ImageProperty class
    describe image's properties in mediateka

---
+++ Mediateka/Mediateka.Core/Concrete/MediaProperties/ImageProperty.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Mediateka.Core.Abstract.Classes;
+
+namespace Mediateka.Core.Concrete.MediaProperties
+{
+    public class ImageProperty : BaseMediaElementProperty
+    {
+        public ImageProperty
+        (
+            string author,
+            DateTime createdDate,
+            string path,
+            byte size,
+            string extension,
+            int width,
+            int height
+        )
+            : base(author, createdDate, path, size, extension)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+    }
+}
