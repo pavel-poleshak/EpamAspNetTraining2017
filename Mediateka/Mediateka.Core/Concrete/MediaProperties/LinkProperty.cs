107afc37e61e5161d6b861a4e5d60eba16176b99
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-08 20:09:11 +0300

    Added class for Links
    contains properties, that describe link

---
+++ Mediateka/Mediateka.Core/Concrete/MediaProperties/LinkProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Mediateka.Core.Abstract.Classes;
+using Mediateka.Core.Abstract.Interfaces;
+
+namespace Mediateka.Core.Concrete.MediaProperties
+{
+    public class LinkProperty:BaseMediaElementProperty
+    {
+        public LinkProperty
+        (
+            string author,
+            DateTime createdDate,
+            string path,
+            byte size,
+            string extension,
+            IMediaElement linkedObject
+        )
+            : base(author, createdDate, path, size, extension)
+        {
+            Object = linkedObject;
+        }
+
+        public IMediaElement Object { get; }
+    }
+}
