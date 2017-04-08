1e8e818b48465bf51054d36375736b2a9f5fba74
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-08 20:07:21 +0300

    Added AudioPropery class
    describe properties for audio files in mediateka

---
+++ Mediateka/Mediateka.Core/Concrete/MediaProperties/AudioProperty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Mediateka.Core.Abstract.Classes;
+
+namespace Mediateka.Core.Concrete.MediaProperties
+{
+    public class AudioProperty : BaseMediaElementProperty
+    {
+        public AudioProperty
+        (
+            string author,
+            DateTime createdDate,
+            string path,
+            byte size,
+            string extension,
+            string genre
+        )
+            : base(author, createdDate, path, size, extension)
+        {
+            Genre = genre;
+        }
+
+        public string Genre { get; }
+    }
+}
