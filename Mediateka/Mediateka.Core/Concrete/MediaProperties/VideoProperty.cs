819117be99ba5a5eda294acbee68f38d411ac60e
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-08 20:08:35 +0300

    Added VideoProperty class
    describe properties for viedo files

---
+++ Mediateka/Mediateka.Core/Concrete/MediaProperties/VideoProperty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Mediateka.Core.Abstract.Classes;
+
+namespace Mediateka.Core.Concrete.MediaProperties
+{
+    public class VideoProperty : BaseMediaElementProperty
+    {
+        public VideoProperty
+        (
+            string author,
+            DateTime createdDate,
+            string path,
+            byte size,
+            string extension,
+            int fps
+        )
+            : base(author, createdDate, path, size, extension)
+        {
+            Fps = fps;
+        }
+
+        public int Fps { get; }
+    }
+}
