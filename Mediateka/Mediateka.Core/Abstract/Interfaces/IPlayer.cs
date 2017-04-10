25a9e39667b963b69b7c6ea07270b7f23f622c94
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-10 23:27:12 +0300

    Added interface for basic player

---
+++ Mediateka/Mediateka.Core/Abstract/Interfaces/IPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateka.Core.Abstract.Interfaces
+{
+    public interface IPlayer
+    {
+        void OpenFile(IMediaElement element);
+        void Close(IMediaElement element);
+        void Play(IMediaElement element);
+        void Stop(IMediaElement element);
+        void Pause(IMediaElement element);
+    }
+}
