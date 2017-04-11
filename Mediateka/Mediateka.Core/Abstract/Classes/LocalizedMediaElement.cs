aa036023e3613489d78e37f18823a6aca6854441
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-11 23:53:32 +0300

    Added class, that describe elements, which have location

---
+++ Mediateka/Mediateka.Core/Abstract/Classes/LocalizedMediaElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateka.Core.Abstract.Classes
+{
+    public abstract class LocalizedMediaElement : BaseMediaElement
+    {
+        protected LocalizedMediaElement(string name, string extension, string path) : base(name)
+        {
+            Extension = extension;
+            Path = path;
+        }
+
+        public string Extension { get; }
+        public string Path { get; }
+    }
+}
