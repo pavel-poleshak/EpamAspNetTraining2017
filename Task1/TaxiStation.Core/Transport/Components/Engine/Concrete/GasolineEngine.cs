e61c67b7d653e8cdd3bcb7c50c58d76005d6b867
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 22:28:35 +0300

    Added class GasolineEngine
    describe gasoline engines in the transport

---
+++ Task1/TaxiStation.Core/Transport/Components/Engine/Concrete/GasolineEngine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+using TaxiStation.Core.Transport.Components.Engine.Abstract;
+using TaxiStation.Core.Transport.Components.Engine.Abstract.Property;
+
+namespace TaxiStation.Core.Transport.Components.Engine.Concrete
+{
+    public class GasolineEngine : CombustionEngine
+    {
+        public GasolineEngine(int engineId, MetaInfo metaInfo, EngineProperty engineProperty)
+            : base(engineId, metaInfo, engineProperty)
+        {
+        }
+
+        public override string Work()
+        {
+            return string.Format("Gasoline Engine working.");
+        }
+    }
+}
