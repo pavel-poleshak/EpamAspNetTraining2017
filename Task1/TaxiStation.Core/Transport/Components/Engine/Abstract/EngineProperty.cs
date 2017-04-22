17e388dfee8b5f8e19d7386a9588b5d7e612f4ba
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 17:21:51 +0300

    Added base class EngineProperty
    Describe base properties of all engines

---
+++ Task1/TaxiStation.Core/Transport/Components/Engine/Abstract/EngineProperty.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Security.Policy;
+
+namespace TaxiStation.Core.Transport.Components.Engine.Abstract
+{
+    public abstract class EngineProperty
+    {
+        public EngineMaterial Material { get; private set; }
+        public int Power { get; private set; }
+    }
+}
