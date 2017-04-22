2bc7db784b1c7cff9fe383d8e809a6d42eba9671
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 17:33:41 +0300

    Added class Model
    describe info about model of component or transport

---
+++ Task1/TaxiStation.Core/MetaData/Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiStation.Core.MetaInfo.Model
+{
+    public class Model
+    {
+        public int ModelId { get; private set; }
+        public string Name { get; private set; }
+    }
+}
