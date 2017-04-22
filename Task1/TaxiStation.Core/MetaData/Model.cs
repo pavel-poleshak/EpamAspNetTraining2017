a7b3f1e517e62b796669714ece08f52745029d11
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 17:37:05 +0300

    Namespace refactoring

--- Task1/TaxiStation.Core/MetaData/Model.cs
+++ Task1/TaxiStation.Core/MetaData/Model.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaxiStation.Core.MetaInfo.Model
+﻿namespace TaxiStation.Core.MetaData
 {
     public class Model
     {
