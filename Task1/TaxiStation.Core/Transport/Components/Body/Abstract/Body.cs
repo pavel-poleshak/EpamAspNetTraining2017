83049966a11f41e4f7268b334959e7606c938606
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 22:38:26 +0300

    Added abstract base class Body
    it class implements interface IBody

---
+++ Task1/TaxiStation.Core/Transport/Components/Body/Abstract/Body.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+
+namespace TaxiStation.Core.Transport.Components.Body.Abstract
+{
+    public class Body : IBody
+    {
+        protected Body(int bodyId, MetaInfo metaInfo)
+        {
+            BodyId = bodyId;
+            MetaInfo = metaInfo;
+        }
+
+        public int BodyId { get; }
+        public MetaInfo MetaInfo { get; }
+    }
+}
