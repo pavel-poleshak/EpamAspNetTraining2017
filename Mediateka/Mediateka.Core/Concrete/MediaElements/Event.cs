5500b8e930cd1219ea3cfc36a9ec41b1fcedf76d
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 09:44:56 +0300

    Event

---
+++ Mediateka/Mediateka.Core/Concrete/MediaElements/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediateka.Core.Abstract.Classes;
+using Mediateka.Core.Abstract.Interfaces;
+
+namespace Mediateka.Core.Concrete.MediaElements
+{
+    public class Event: BaseMediaElement, IMediaElement
+    {
+        public Event(string name) : base(name)
+        {
+            
+        }
+        public string Play()
+        {
+            return string.Format("Event playing: {0}", Name);
+        }
+    }
+}
