35476d09e5e71b47b9c036a6f574979c7ff181cc
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 18:03:06 +0300

    Added class CombustionEngineProperty
    Contains some info about combustion engines

---
+++ Task1/TaxiStation.Core/Transport/Components/Engine/Concrete/Properties/CombustionEngineProperty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.Transport.Components.Engine.Abstract.Property;
+
+namespace TaxiStation.Core.Transport.Components.Engine.Concrete.Properties
+{
+    public class CombustionEngineProperty : EngineProperty
+    {
+        public CombustionEngineProperty(EngineMaterial material, int power, int torque, int capacity, int cylindersCount) 
+            : base(material, power, torque)
+        {
+            Capacity = capacity;
+            CylindersCount = cylindersCount;
+        }
+
+        public int Capacity { get; private set; }
+        public int CylindersCount { get; private set; }
+    }
+}
