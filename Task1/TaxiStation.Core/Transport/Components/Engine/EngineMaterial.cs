377feea3a228adaf82b0ff51c93855b070705fe7
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 13:53:20 +0300

    Added enum EngineMaterial
    Describe material of engine

---
+++ Task1/TaxiStation.Core/Transport/Components/Engine/EngineMaterial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiStation.Core.Transport.Components.Engine
+{
+    public enum EngineMaterial
+    {
+        CastIron,
+        Aluminum,
+        Magnesium
+    }
+}
