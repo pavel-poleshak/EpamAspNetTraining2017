fb1e079c11084414003fdbdf7dc26ca57277a6c1
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 13:41:52 +0300

    Added interface IEngine

---
+++ Task1/TaxiStation.Core/Transport/Components/Engine/Abstract/IEngine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiStation.Core.Transport.Components.Engine.Abstract
+{
+    public interface IEngine
+    {
+        int EngineId { get; }
+        MetaInfo MetaInfo { get; }
+        EngineProperty EngineProperty { get; }
+    }
+}
