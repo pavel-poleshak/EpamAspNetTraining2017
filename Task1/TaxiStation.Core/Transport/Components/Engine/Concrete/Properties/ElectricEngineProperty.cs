1af04c76698e1ff6ca2f7675d266fc76a3b39fb8
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 19:35:58 +0300

    Added class ElectricEngineProperty
    contains properties for electric engines

---
+++ Task1/TaxiStation.Core/Transport/Components/Engine/Concrete/Properties/ElectricEngineProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.Transport.Components.Engine.Abstract.Property;
+
+namespace TaxiStation.Core.Transport.Components.Engine.Concrete.Properties
+{
+    public class ElectricEngineProperty:EngineProperty
+    {
+        public ElectricEngineProperty(EngineMaterial material, int power, int torque, int ratedVoltage) 
+            : base(material, power, torque)
+        {
+            RatedVoltage = ratedVoltage;
+        }
+
+        public int RatedVoltage { get; private set; }
+    }
+}
