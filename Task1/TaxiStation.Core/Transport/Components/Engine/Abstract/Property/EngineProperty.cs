83e3f3698fd26c5650b86824689fe57b77902acc
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 17:51:47 +0300

    renamed name space, added constructor to the class EngineProperty

---
+++ Task1/TaxiStation.Core/Transport/Components/Engine/Abstract/Property/EngineProperty.cs
@@ -0,0 +1,14 @@
+﻿namespace TaxiStation.Core.Transport.Components.Engine.Abstract.Property
+{
+    public abstract class EngineProperty
+    {
+        protected EngineProperty(EngineMaterial material, int power)
+        {
+            Material = material;
+            Power = power;
+        }
+
+        public EngineMaterial Material { get; private set; }
+        public int Power { get; private set; }
+    }
+}
