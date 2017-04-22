2cd49d69e34d578198d27cc4401a44c56077bf37
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 17:57:37 +0300

    Added Torque property to the basic class EngineProperty

--- Task1/TaxiStation.Core/Transport/Components/Engine/Abstract/Property/EngineProperty.cs
+++ Task1/TaxiStation.Core/Transport/Components/Engine/Abstract/Property/EngineProperty.cs
@@ -2,13 +2,15 @@
 {
     public abstract class EngineProperty
     {
-        protected EngineProperty(EngineMaterial material, int power)
+        protected EngineProperty(EngineMaterial material, int power, int torque)
         {
             Material = material;
             Power = power;
+            Torque = torque;
         }
 
         public EngineMaterial Material { get; private set; }
         public int Power { get; private set; }
+        public int Torque { get; private set; }
     }
 }
