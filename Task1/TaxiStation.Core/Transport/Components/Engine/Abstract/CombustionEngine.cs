12127ccbf22862fbd73a2be25ead76acebf58333
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 22:27:52 +0300

    Added abstract class CombustionEngine
    describe all combustion engines

---
+++ Task1/TaxiStation.Core/Transport/Components/Engine/Abstract/CombustionEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+using TaxiStation.Core.Transport.Components.Engine.Abstract.Property;
+
+namespace TaxiStation.Core.Transport.Components.Engine.Abstract
+{
+    public abstract class CombustionEngine : Engine
+    {
+        protected CombustionEngine(int engineId, MetaInfo metaInfo, EngineProperty engineProperty)
+            : base(engineId, metaInfo, engineProperty)
+        {
+        }
+
+        public override string Work()
+        {
+            return string.Format("Combustion Engine working.");
+        }
+    }
+}
