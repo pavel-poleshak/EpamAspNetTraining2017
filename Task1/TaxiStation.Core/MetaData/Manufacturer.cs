9d93052a7a00e0a1331308ba0bc6ca864a38b484
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 17:34:16 +0300

    Added class Manufacturer
    describe info about manufacturer of components or transport

---
+++ Task1/TaxiStation.Core/MetaData/Manufacturer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiStation.Core.Manufacturer
+{
+    public class Manufacturer
+    {
+        public int ManufacturerId { get; private set; }
+        public string Name { get; private set; }
+        public string Country { get; private set; }
+    }
+}
