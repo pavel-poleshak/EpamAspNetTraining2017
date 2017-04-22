44dee610f892f9366a165db6bb8ac76a890e8fa5
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 22:07:48 +0300

    Added abstract base class for Engine

---
+++ Task1/TaxiStation.Core/Transport/Components/Engine/Abstract/Engine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+using TaxiStation.Core.Transport.Components.Engine.Abstract.Property;
+
+namespace TaxiStation.Core.Transport.Components.Engine.Abstract
+{
+    public abstract class Engine : IEngine
+    {
+        protected Engine(int engineId, MetaInfo metaInfo, EngineProperty engineProperty)
+        {
+            EngineId = engineId;
+            MetaInfo = metaInfo;
+            EngineProperty = engineProperty;
+        }
+
+        public int EngineId { get; }
+        public MetaInfo MetaInfo { get; }
+        public EngineProperty EngineProperty { get; }
+    }
+}
