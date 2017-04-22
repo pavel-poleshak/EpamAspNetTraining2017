6c5d9b017b53eac94cde2e34d74191b53409e4c6
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-22 17:35:20 +0300

    Added class MetaInfo
    contains info about manufacturer, model and creating date

---
+++ Task1/TaxiStation.Core/MetaData/MetaInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TaxiStation.Core.MetaData
+{
+    public class MetaInfo
+    {
+        public Manufacturer Manufacturer { get; private set; }
+        public Model Model { get; private set; }
+        public DateTime CreatingDate { get; private set; }
+    }
+}
