8e019dea758b82a6a2f5dd748bae34f47e894bd4
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-23 16:08:08 +0300

    Added base class Transmission
    implements interface ITransmission

---
+++ Task1/TaxiStation.Core/Transport/Components/TransmissionModel/Abstract/Transmission.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+
+namespace TaxiStation.Core.Transport.Components.TransmissionModel.Abstract
+{
+    public class Transmission:ITransmission
+    {
+        protected Transmission(int transmissionId, MetaInfo metaInfo, int numberOfGears)
+        {
+            TransmissionId = transmissionId;
+            MetaInfo = metaInfo;
+            NumberOfGears = numberOfGears;
+        }
+
+        public int TransmissionId { get; }
+        public MetaInfo MetaInfo { get; }
+        public int NumberOfGears { get; }
+    }
+}
