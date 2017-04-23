b7768739e7f74def0c335cc7dc679762874ae0b6
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-23 16:07:42 +0300

    Added interface ITransmission for transmission objects

---
+++ Task1/TaxiStation.Core/Transport/Components/TransmissionModel/Abstract/ITransmission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+
+namespace TaxiStation.Core.Transport.Components.TransmissionModel.Abstract
+{
+    public interface ITransmission
+    {
+        int TransmissionId { get; }
+        MetaInfo MetaInfo { get; }
+        int NumberOfGears { get; }
+    }
+}
