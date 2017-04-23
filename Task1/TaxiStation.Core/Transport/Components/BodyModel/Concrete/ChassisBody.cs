b09f02795615e9768b7b10c212fee6f7562ce256
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-23 14:40:01 +0300

    Added class ChassisBody
    implements base class Body

---
+++ Task1/TaxiStation.Core/Transport/Components/BodyModel/Concrete/ChassisBody.cs
@@ -0,0 +1,12 @@
+﻿using TaxiStation.Core.MetaData;
+using TaxiStation.Core.Transport.Components.BodyModel.Abstract;
+
+namespace TaxiStation.Core.Transport.Components.BodyModel.Concrete
+{
+    public class ChassisBody : Body
+    {
+        public ChassisBody(int bodyId, MetaInfo metaInfo) : base(bodyId, metaInfo)
+        {
+        }
+    }
+}
