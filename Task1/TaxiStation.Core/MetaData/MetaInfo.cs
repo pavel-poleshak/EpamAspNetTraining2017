0b9da20fe350ec2fe1af75d399cce9070a094dd9
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-23 16:06:55 +0300

    Added property Weight to the class MetaInfo

--- Task1/TaxiStation.Core/MetaData/MetaInfo.cs
+++ Task1/TaxiStation.Core/MetaData/MetaInfo.cs
@@ -7,5 +7,6 @@ namespace TaxiStation.Core.MetaData
         public Manufacturer Manufacturer { get; private set; }
         public Model Model { get; private set; }
         public DateTime CreatingDate { get; private set; }
+        public int Weight { get; private set; }
     }
 }
