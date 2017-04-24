e0ee1c297454b0f4c9427b4d28dd97838e2c7871
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:18:11 +0300

    Added interface IRim
    contains information about Rim

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/RimModel/Abstract/IRim.cs
@@ -0,0 +1,11 @@
+﻿using TaxiStation.Core.MetaData;
+
+namespace TaxiStation.Core.Transport.Components.WheelModel.RimModel.Abstract
+{
+    public interface IRim
+    {
+        int RimId { get; }
+        MetaInfo MetaInfo { get; }
+        RimProperty Property { get; }
+    }
+}
