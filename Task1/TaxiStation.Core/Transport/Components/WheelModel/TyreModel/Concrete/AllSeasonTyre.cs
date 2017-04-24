b7741a54c0841368e1fdc2db162aee9c520b7918
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:21:08 +0300

    Addded class AllSeasonTyre
    implements Tyre

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/TyreModel/Concrete/AllSeasonTyre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+using TaxiStation.Core.Transport.Components.WheelModel.TyreModel.Abstract;
+
+namespace TaxiStation.Core.Transport.Components.WheelModel.TyreModel.Concrete
+{
+    public class AllSeasonTyre:Tyre
+    {
+        public AllSeasonTyre(int tyreId, MetaInfo metaInfo, TyreProperty property) : base(tyreId, metaInfo, property)
+        {
+        }
+    }
+}
