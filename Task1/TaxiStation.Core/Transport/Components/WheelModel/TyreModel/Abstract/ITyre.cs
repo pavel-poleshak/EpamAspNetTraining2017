2422ca570fbaeef981f1c7204f84cd074c13b4d6
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:20:24 +0300

    Added interface for Tyre

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/TyreModel/Abstract/ITyre.cs
@@ -0,0 +1,11 @@
+﻿using TaxiStation.Core.MetaData;
+
+namespace TaxiStation.Core.Transport.Components.WheelModel.TyreModel.Abstract
+{
+    public interface ITyre
+    {
+        int TyreId { get; }
+        MetaInfo MetaInfo { get; }
+        TyreProperty Property { get; }
+    }
+}
