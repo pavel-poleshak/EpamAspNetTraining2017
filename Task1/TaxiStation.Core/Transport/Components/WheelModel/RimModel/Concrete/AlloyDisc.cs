e50a29d64b6c74ee596b26004cd3172dfdd437d8
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:19:07 +0300

    Added class AlloyDisc
    implements class Rim

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/RimModel/Concrete/AlloyDisc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+using TaxiStation.Core.Transport.Components.WheelModel.RimModel.Abstract;
+
+namespace TaxiStation.Core.Transport.Components.WheelModel.RimModel.Concrete
+{
+    public class AlloyDisc : Rim
+    {
+        public AlloyDisc(int rimId, MetaInfo metaInfo, RimProperty property) 
+            : base(rimId, metaInfo, property)
+        {
+        }
+    }
+}
