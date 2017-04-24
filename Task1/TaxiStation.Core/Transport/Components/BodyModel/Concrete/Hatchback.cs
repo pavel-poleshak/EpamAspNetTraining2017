1c61023afad6a8bf82aec5eb794671398d86b90d
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:16:32 +0300

    Added class Hatchback
    implements BodyClass

---
+++ Task1/TaxiStation.Core/Transport/Components/BodyModel/Concrete/Hatchback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.Transport.Components.BodyModel.Abstract;
+
+namespace TaxiStation.Core.Transport.Components.BodyModel.Concrete
+{
+    public class Hatchback:BodyClass
+    {
+        public Hatchback(int bodyClassId) : base(bodyClassId)
+        {
+        }
+    }
+}
