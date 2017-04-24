2df1c11f9c35f811e83d6b2263cd4c80fbc6be8d
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:21:27 +0300

    Added class WinterTyre
    implements class Tyre

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/TyreModel/Concrete/WinterTyre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+using TaxiStation.Core.Transport.Components.WheelModel.TyreModel.Abstract;
+
+namespace TaxiStation.Core.Transport.Components.WheelModel.TyreModel.Concrete
+{
+    public class WinterTyre:Tyre
+    {
+        public WinterTyre(int tyreId, MetaInfo metaInfo, TyreProperty property) : base(tyreId, metaInfo, property)
+        {
+        }
+    }
+}
