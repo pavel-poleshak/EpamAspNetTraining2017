bac96a556ec38c2b45fcefeebd930d668101dd8e
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:00:21 +0300

    Added class BodyClass
    describe class of body. ex.: sedan

---
+++ Task1/TaxiStation.Core/Transport/Components/BodyModel/Abstract/BodyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiStation.Core.Transport.Components.BodyModel.Abstract
+{
+    public abstract class BodyClass : IBodyClass
+    {
+        protected BodyClass(int bodyClassId)
+        {
+            BodyClassId = bodyClassId;
+        }
+
+        public int BodyClassId { get; private set; }
+    }
+}
