ac55aeef2fd561b6dbc92d1a7741c2cb7513ee06
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:20:42 +0300

    Added base class Tyre
    abstract class

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/TyreModel/Abstract/Tyre.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+
+namespace TaxiStation.Core.Transport.Components.WheelModel.TyreModel.Abstract
+{
+    public abstract class Tyre:ITyre
+    {
+        protected Tyre(int tyreId, MetaInfo metaInfo, TyreProperty property)
+        {
+            TyreId = tyreId;
+            MetaInfo = metaInfo;
+            Property = property;
+        }
+
+        public int TyreId { get; private set; }
+        public MetaInfo MetaInfo { get; private set; }
+        public TyreProperty Property { get; private set; }
+    }
+}
