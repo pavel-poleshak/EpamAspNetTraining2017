1839d8b87dd243f55ea39f7470e0aea048fe040e
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 17:25:33 +0300

    Added class PassengerCar
    implements Car, IPassenegerCar

---
+++ Task1/TaxiStation.Core/Transport/PassengerCar.cs
@@ -0,0 +1,15 @@
+﻿using TaxiStation.Core.MetaData;
+using TaxiStation.Core.Transport.Abstract.CarModel;
+
+namespace TaxiStation.Core.Transport
+{
+    public class PassengerCar : Car, IPassengerCar
+    {
+        public PassengerCar(int transportId, MetaInfo metaInfo, int countOfSeat) : base(transportId, metaInfo)
+        {
+            CountOfSeat = countOfSeat;
+        }
+
+        public int CountOfSeat { get; }
+    }
+}
