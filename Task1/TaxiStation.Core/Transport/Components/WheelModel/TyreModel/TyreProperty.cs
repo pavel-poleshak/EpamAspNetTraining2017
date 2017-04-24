97767084dde78123c88b2de8c84d7362ae1fe27f
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:21:50 +0300

    Added struct for tyre's properties

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/TyreModel/TyreProperty.cs
@@ -0,0 +1,20 @@
+﻿namespace TaxiStation.Core.Transport.Components.WheelModel.TyreModel
+{
+    public struct TyreProperty
+    {
+        public TyreProperty(int width, int height, int radius, int loadIndex, int speedIndex)
+        {
+            Width = width;
+            Height = height;
+            Radius = radius;
+            LoadIndex = loadIndex;
+            SpeedIndex = speedIndex;
+        }
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int Radius { get; private set; }
+        public int LoadIndex { get; private set; }
+        public int SpeedIndex { get; private set; }
+    }
+}
