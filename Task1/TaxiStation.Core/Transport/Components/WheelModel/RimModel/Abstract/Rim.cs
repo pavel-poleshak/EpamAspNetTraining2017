bb57018fe17e5dae31f0ea04eacdf0804fd99763
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:18:35 +0300

    Added base abstract class Rim
    Base class for Rim

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/RimModel/Abstract/Rim.cs
@@ -0,0 +1,18 @@
+﻿using TaxiStation.Core.MetaData;
+
+namespace TaxiStation.Core.Transport.Components.WheelModel.RimModel.Abstract
+{
+    public abstract class Rim : IRim
+    {
+        protected Rim(int rimId, MetaInfo metaInfo, RimProperty property)
+        {
+            RimId = rimId;
+            MetaInfo = metaInfo;
+            Property = property;
+        }
+
+        public int RimId { get; private set; }
+        public MetaInfo MetaInfo { get; private set; }
+        public RimProperty Property { get; private set; }
+    }
+}
