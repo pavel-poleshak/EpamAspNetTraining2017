965609ea87531cdb4c7ee6d77d14108fca89cda7
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 17:23:22 +0300

    Added abstract class BaseTransport

---
+++ Task1/TaxiStation.Core/Transport/Abstract/BaseTransport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+
+namespace TaxiStation.Core.Transport.Abstract
+{
+    public abstract class BaseTransport : ITransport
+    {
+        protected BaseTransport(int transportId, MetaInfo metaInfo)
+        {
+            TransportId = transportId;
+            MetaInfo = metaInfo;
+        }
+
+        public int TransportId { get; private set; }
+        public MetaInfo MetaInfo { get; private set; }
+    }
+}
