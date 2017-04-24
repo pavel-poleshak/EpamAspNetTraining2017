25ad726d84276de832b5afed939a18dbe98fdfeb
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:17:34 +0300

    Added interface IWheel
    Contains informarion about wheel, rim and tyre

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/Abstract/IWheel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.Transport.Components.WheelModel.RimModel.Abstract;
+using TaxiStation.Core.Transport.Components.WheelModel.TyreModel.Abstract;
+
+namespace TaxiStation.Core.Transport.Components.WheelModel.Abstract
+{
+    public interface IWheel
+    {
+        int WheelId { get; }
+        IRim Rim { get; }
+        ITyre Tyre { get; }
+    }
+}
