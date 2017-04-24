50cde2e86a1fe64079ebd0bedccaceaebf06ee3c
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 17:24:01 +0300

    Added abstract class Car
    implements BaseTransport

---
+++ Task1/TaxiStation.Core/Transport/Abstract/CarModel/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+
+namespace TaxiStation.Core.Transport.Abstract.CarModel
+{
+    public abstract class Car : BaseTransport
+    {
+        protected Car(int transportId, MetaInfo metaInfo) : base(transportId, metaInfo)
+        {
+        }
+    }
+}
