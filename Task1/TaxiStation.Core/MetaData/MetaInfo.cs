b418b5a8056374415f26987173f84753c22eb76e
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 17:23:03 +0300

    Changes in MetaInfo class
    added constructor

--- Task1/TaxiStation.Core/MetaData/MetaInfo.cs
+++ Task1/TaxiStation.Core/MetaData/MetaInfo.cs
@@ -4,6 +4,14 @@ namespace TaxiStation.Core.MetaData
 {
     public class MetaInfo
     {
+        public MetaInfo(Manufacturer manufacturer, Model model, DateTime creatingDate, int weight)
+        {
+            Manufacturer = manufacturer;
+            Model = model;
+            CreatingDate = creatingDate;
+            Weight = weight;
+        }
+
         public Manufacturer Manufacturer { get; private set; }
         public Model Model { get; private set; }
         public DateTime CreatingDate { get; private set; }
