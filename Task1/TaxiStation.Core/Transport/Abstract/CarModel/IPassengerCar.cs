3211f0883a1af4176f61fee08d426384cd1ff8d5
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 17:24:42 +0300

    Added interface IPassenegerCar
    describe CountOfSeat property

---
+++ Task1/TaxiStation.Core/Transport/Abstract/CarModel/IPassengerCar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiStation.Core.Transport.Abstract.CarModel
+{
+    public interface IPassengerCar : ITransport
+    {
+        int CountOfSeat { get; }
+    }
+}
