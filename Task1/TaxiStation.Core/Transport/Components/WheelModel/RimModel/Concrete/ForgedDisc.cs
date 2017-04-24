81a6dff83db9bd2ec3354c6186d95a9c52d88b2d
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:19:27 +0300

    Added class ForgedDisc
    Implements class Rim

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/RimModel/Concrete/ForgedDisc.cs
@@ -0,0 +1,13 @@
+﻿using TaxiStation.Core.MetaData;
+using TaxiStation.Core.Transport.Components.WheelModel.RimModel.Abstract;
+
+namespace TaxiStation.Core.Transport.Components.WheelModel.RimModel.Concrete
+{
+    public class ForgedDisc : Rim
+    {
+        public ForgedDisc(int rimId, MetaInfo metaInfo, RimProperty property)
+            : base(rimId, metaInfo, property)
+        {
+        }
+    }
+}
