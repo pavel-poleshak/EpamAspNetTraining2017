c8637f28f2f3e89721466f154d603df708eb7707
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:19:59 +0300

    Added struct for rim properties
    describe main properties of the rim

---
+++ Task1/TaxiStation.Core/Transport/Components/WheelModel/RimModel/RimProperty.cs
@@ -0,0 +1,10 @@
+﻿namespace TaxiStation.Core.Transport.Components.WheelModel.RimModel
+{
+    public struct RimProperty
+    {
+        public double Width { get; private set; }
+        public int Diameter { get; private set; }
+        public double PCD { get; private set; }
+        public double EinpressTief { get; private set; }
+    }
+}
