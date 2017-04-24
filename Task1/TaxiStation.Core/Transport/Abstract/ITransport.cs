0de9f919bd34c3b67ada7c234974af9f35c8d854
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:41:55 +0300

    Added interface ITransport

---
+++ Task1/TaxiStation.Core/Transport/Abstract/ITransport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.MetaData;
+
+namespace TaxiStation.Core.Transport.Abstract
+{
+    public  interface ITransport
+    {
+        int TransportId { get; }
+        MetaInfo MetaInfo { get; }
+    }
+}
