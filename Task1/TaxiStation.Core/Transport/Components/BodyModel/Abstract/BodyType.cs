00a80e5619e0dd3ad48fae739be5ac0e99d40f12
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-24 16:01:01 +0300

    Added class BodyType
    describe type of body. ex.:chassis

---
+++ Task1/TaxiStation.Core/Transport/Components/BodyModel/Abstract/BodyType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiStation.Core.Transport.Components.BodyModel.Abstract
+{
+    public abstract class BodyType : IBodyType
+    {
+        protected BodyType(int bodyTypeId)
+        {
+            BodyTypeId = bodyTypeId;
+        }
+
+        public int BodyTypeId { get; }
+    }
+}
