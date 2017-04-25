afe7148b00daa438f8230869fe6804e951343b1e
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-25 17:52:26 +0300

    Added struct FuelConsumption

---
+++ Task1/TaxiStation.Core/MetaData/FuelConsumption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiStation.Core.MetaData
+{
+    public struct FuelConsumption
+    {
+        public FuelConsumption(double cityCycle, double trackCycle)
+        {
+            CityCycle = cityCycle;
+            TrackCycle = trackCycle;
+        }
+
+        public double CityCycle { get; private set; }
+        public double TrackCycle { get; private set; }
+    }
+}
