e1ce1b480f32913f815a65d7a2451466f31402bd
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-25 17:56:26 +0300

    Added class CarComponent
    include all components of car

---
+++ Task1/TaxiStation.Core/Transport/Components/CarComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiStation.Core.Transport.Components.BodyModel.Abstract;
+using TaxiStation.Core.Transport.Components.EngineModel.Abstract;
+using TaxiStation.Core.Transport.Components.TransmissionModel.Abstract;
+using TaxiStation.Core.Transport.Components.WheelModel.Abstract;
+
+namespace TaxiStation.Core.Transport.Components
+{
+    public class CarComponent
+    {
+        IBody Body { get; }
+        IEngine Engine { get; }
+        ITransmission Transmission { get; }
+        ICollection<IWheel> Wheels { get; }
+    }
+}
