b5207270bb4f320d76eb34a482267477e97ff83a
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-25 17:55:11 +0300

    Refactored class Car

--- Task1/TaxiStation.Core/Transport/Abstract/CarModel/Car.cs
+++ Task1/TaxiStation.Core/Transport/Abstract/CarModel/Car.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TaxiStation.Core.MetaData;
+using TaxiStation.Core.Transport.Components;
+using TaxiStation.Core.Transport.Components.BodyModel.Abstract;
+using TaxiStation.Core.Transport.Components.EngineModel.Abstract;
+using TaxiStation.Core.Transport.Components.TransmissionModel.Abstract;
+using TaxiStation.Core.Transport.Components.WheelModel.Abstract;
 
 namespace TaxiStation.Core.Transport.Abstract.CarModel
 {
-    public abstract class Car : BaseTransport
+    public abstract class Car : BaseTransport, ICar
     {
-        protected Car(int transportId, MetaInfo metaInfo) : base(transportId, metaInfo)
+        protected Car(int transportId, MetaInfo metaInfo, CarComponent component) 
+            : base(transportId, metaInfo)
         {
+            Component = component;
         }
+
+        public CarComponent Component { get; }
     }
 }
