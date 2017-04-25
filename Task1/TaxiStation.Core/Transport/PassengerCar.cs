fbcdfdfb6b81e50c63d665fe6ab2ca87162e6ca3
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-25 17:55:52 +0300

    Refactored class PassengerCar

--- Task1/TaxiStation.Core/Transport/PassengerCar.cs
+++ Task1/TaxiStation.Core/Transport/PassengerCar.cs
@@ -1,11 +1,12 @@
 ﻿using TaxiStation.Core.MetaData;
 using TaxiStation.Core.Transport.Abstract.CarModel;
+using TaxiStation.Core.Transport.Components;
 
 namespace TaxiStation.Core.Transport
 {
     public class PassengerCar : Car, IPassengerCar
     {
-        public PassengerCar(int transportId, MetaInfo metaInfo, int countOfSeat) : base(transportId, metaInfo)
+        public PassengerCar(int transportId, MetaInfo metaInfo, CarComponent component, int countOfSeat) : base(transportId, metaInfo, component)
         {
             CountOfSeat = countOfSeat;
         }
