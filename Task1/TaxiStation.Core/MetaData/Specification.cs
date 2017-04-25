dc71c5b49c510bdc596fc569af72536bbf5a6fb5
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-04-25 17:53:56 +0300

    Added class Specification
    Include all tech specifications

---
+++ Task1/TaxiStation.Core/MetaData/Specification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiStation.Core.MetaData
+{
+    public class Specification
+    {
+        public int Weight { get; private set; }
+        public FuelConsumption FuelConsumption { get; private set; }
+        public int Speed { get; private set; }
+    }
+}
