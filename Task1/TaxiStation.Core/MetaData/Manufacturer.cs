374b1be7133dac2c5c3f90a059d9bc69dec2c39e
Pavel Poleshak <mr.gorden.beat@gmail.com>
2017-05-02 00:42:37 +0300

    added constructor to the class Manufacturer

--- Task1/TaxiStation.Core/MetaData/Manufacturer.cs
+++ Task1/TaxiStation.Core/MetaData/Manufacturer.cs
@@ -2,6 +2,13 @@
 {
     public class Manufacturer
     {
+        public Manufacturer(int id, string name, string country)
+        {
+            ManufacturerId = id;
+            Name = name;
+            Country = country;
+        }
+
         public int ManufacturerId { get; private set; }
         public string Name { get; private set; }
         public string Country { get; private set; }
